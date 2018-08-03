3c6244878dfbf6bdc20084f994c589f7ed55c918
DESKTOP-R6T0CSH\Pc <gameraroma@gmail.com>
2018-08-03 14:31:01 +0700

    edit little bit

--- app/src/main/java/gameraromic/uitemplate/LoginPageListActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/LoginPageListActivity.kt
@@ -1,19 +1,38 @@
 package gameraromic.uitemplate
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.widget.SimpleCursorAdapter
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import gameraromic.uitemplate.exampleviews.LoginLightActivity
 import kotlinx.android.synthetic.main.activity_login_page_list.*
 
 class LoginPageListActivity : AppCompatActivity() {
+
+    private val LightTheme: String = "Light"
+    private val DarkTheme: String = "Dark"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page_list)
 
-        val loginList = arrayOf("light", "dark")
+        val loginList = arrayOf(LightTheme, DarkTheme)
 
         loginPageListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1 , loginList)
+
+        loginPageListView.setOnItemClickListener { adapterView, view, i, l ->
+            val itemTextView = view.findViewById<TextView>(android.R.id.text1)
+            when (itemTextView.text) {
+                LightTheme -> {
+                    val intent = Intent(this, LoginLightActivity::class.java)
+                    startActivity(intent)
+                }
+                DarkTheme -> {
+
+                }
+                else -> {}
+            }
+        }
     }
 }
