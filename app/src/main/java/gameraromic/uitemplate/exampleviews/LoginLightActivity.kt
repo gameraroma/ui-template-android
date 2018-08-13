d8a57c6924512a9210823ab07d3e3ef0c13b24d2
DESKTOP-R6T0CSH\Pc <gameraroma@gmail.com>
2018-08-13 16:07:39 +0700

    add login edit text validation

--- app/src/main/java/gameraromic/uitemplate/exampleviews/LoginLightActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/exampleviews/LoginLightActivity.kt
@@ -7,16 +7,25 @@ import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import gameraromic.uitemplate.R
 import kotlinx.android.synthetic.main.activity_login_light.*
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+
 
 class LoginLightActivity : AppCompatActivity() {
+    private val emailString: String = "a@a.co"
+    private val passwordString: String = "1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_light)
 
-        login_button.setOnClickListener { it ->
+        login_button.setOnClickListener { _ ->
             loginAction()
         }
+
+        email_edit_text_layout.hint = email_edit_text_layout.hint.toString() + ": " + emailString
+        password_edit_text_layout.hint = password_edit_text_layout.hint.toString() + ": " + passwordString
 
         password_edit_text.setOnEditorActionListener OnEditorActionListener@{ textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE || keyEvent?.keyCode == KeyEvent.KEYCODE_ENTER)
@@ -30,13 +39,67 @@ class LoginLightActivity : AppCompatActivity() {
         registerButtonText.setOnClickListener {
             registerAction()
         }
+
+        email_edit_text.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                email_edit_text_layout.error = null
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        password_edit_text.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                password_edit_text_layout.error = null
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
     }
 
     private fun loginAction() {
-        Toast.makeText(this, "Login!", Toast.LENGTH_SHORT).show()
+        email_edit_text_layout.error = null
+        password_edit_text_layout.error = null
+
+        val emailText = email_edit_text.text.toString()
+        val passwordText = password_edit_text.text.toString()
+
+        val isValidEmail = emailText == emailString
+        val isValidPassword = passwordText == passwordString
+
+        if (emailText.isEmpty()) {
+            email_edit_text_layout.error = "Email cannot be empty"
+        } else if (!isEmailFormatValid(emailText)) {
+            email_edit_text_layout.error = "Wrong format"
+        } else if (!isValidEmail) {
+            email_edit_text_layout.error = "Incorrect Email"
+        }
+
+        if (passwordText.isEmpty()) {
+            password_edit_text_layout.error = "Password cannot be empty"
+        } else if (!isValidPassword) {
+            password_edit_text_layout.error = "Incorrect Password"
+        }
+
+        if (isValidEmail && isValidPassword) {
+            Toast.makeText(this, "Login!", Toast.LENGTH_SHORT).show()
+            return
+        }
     }
 
     private fun registerAction() {
         Toast.makeText(this, "Register!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isEmailFormatValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
