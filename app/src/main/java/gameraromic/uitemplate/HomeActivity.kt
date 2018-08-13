ecf1f4179f27fe3330807d72e580244f0d211682
DESKTOP-R6T0CSH\Pc <gameraroma@gmail.com>
2018-08-13 13:37:56 +0700

    finish login

--- app/src/main/java/gameraromic/uitemplate/HomeActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/HomeActivity.kt
@@ -13,7 +13,6 @@ class HomeActivity : AppCompatActivity() {
 
     private val _loginSection: String = "Login"
     private val _listSection: String = "List"
-    private val _loginList = arrayOf(_loginSection, _listSection)
     private val _menuItemList = arrayOf(MenuItem(_loginSection, R.drawable.ic_lock_open_outline), MenuItem(_listSection, R.drawable.ic_format_list_bulleted))
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/gameraromic/uitemplate/exampleviews/LoginLightActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/exampleviews/LoginLightActivity.kt
@@ -2,12 +2,41 @@ package gameraromic.uitemplate.exampleviews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import gameraromic.uitemplate.R
+import kotlinx.android.synthetic.main.activity_login_light.*
 
 class LoginLightActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_light)
+
+        login_button.setOnClickListener { it ->
+            loginAction()
+        }
+
+        password_edit_text.setOnEditorActionListener OnEditorActionListener@{ textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_DONE || keyEvent?.keyCode == KeyEvent.KEYCODE_ENTER)
+            {
+                loginAction()
+                return@OnEditorActionListener true
+            }
+            return@OnEditorActionListener false
+        }
+
+        registerButtonText.setOnClickListener {
+            registerAction()
+        }
+    }
+
+    private fun loginAction() {
+        Toast.makeText(this, "Login!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun registerAction() {
+        Toast.makeText(this, "Register!", Toast.LENGTH_SHORT).show()
     }
 }
