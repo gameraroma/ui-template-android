a5a9d73daa672355348a26d605508e052cb75bcb
Chakpiwat Pacharajindawat <gameraroma@gmail.com>
2018-09-15 13:51:39 +0700

    add list wallpaper

---
+++ app/src/main/java/gameraromic/uitemplate/ExampleData.kt
@@ -0,0 +1,16 @@
+package gameraromic.uitemplate
+
+import gameraromic.uitemplate.models.List.GirlGroupMember
+
+val getMembersList: Array<GirlGroupMember> =
+        arrayOf(
+                GirlGroupMember("Irene", R.drawable.rvv_irene, "Red Velvet", "Leader, Lead Dancer, Rapper, Visual"),
+                GirlGroupMember("Seulgi", R.drawable.rvv_seulgi, "Red Velvet", "Main Dancer, Vocal"),
+                GirlGroupMember("Wendy", R.drawable.rvv_wendy, "Red Velvet", "Main Vocal"),
+                GirlGroupMember("Jisoo", R.drawable.bp_jisoo, "BLACKPINK", "Lead Vocal"),
+                GirlGroupMember("Jennie", R.drawable.bp_jenny, "BLACKPINK", "Main Rapper, Support Vocal"),
+                GirlGroupMember("Joy", R.drawable.rvv_joy, "Red Velvet", "Main Rapper, Support Vocal"),
+                GirlGroupMember("Rosé", R.drawable.bp_rose, "BLACKPINK", "Main Vocal, Lead Dancer"),
+                GirlGroupMember("Lisa", R.drawable.bp_lisa, "BLACKPINK", "Main Rapper, Support Vocal"),
+                GirlGroupMember("Yeri",R.drawable.rvv_yeri, "Red Velvet", "Rapper, Support Vocal")
+        )
--- app/src/main/java/gameraromic/uitemplate/exampleviews/ListLightActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/exampleviews/ListLightActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import gameraromic.uitemplate.R
 import gameraromic.uitemplate.adapters.ListLightAdapter
+import gameraromic.uitemplate.getMembersList
 import gameraromic.uitemplate.models.List.GirlGroupMember
 import kotlinx.android.synthetic.main.activity_list_light.*
 
@@ -19,17 +20,7 @@ class ListLightActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_light)
 
-        membersList = arrayOf(
-                GirlGroupMember("Irene", R.drawable.rvv_irene, "Red Velvet", "Leader, Lead Dancer, Rapper, Visual"),
-                GirlGroupMember("Seulgi", R.drawable.rvv_seulgi, "Red Velvet", "Main Dancer, Vocal"),
-                GirlGroupMember("Wendy", R.drawable.rvv_wendy, "Red Velvet", "Main Vocal"),
-                GirlGroupMember("Jisoo", R.drawable.bp_jisoo, "BLACKPINK", "Lead Vocal"),
-                GirlGroupMember("Jennie", R.drawable.bp_jenny, "BLACKPINK", "Main Rapper, Support Vocal"),
-                GirlGroupMember("Joy", R.drawable.rvv_joy, "Red Velvet", "Main Rapper, Support Vocal"),
-                GirlGroupMember("Rosé", R.drawable.bp_rose, "BLACKPINK", "Main Vocal, Lead Dancer"),
-                GirlGroupMember("Lisa", R.drawable.bp_lisa, "BLACKPINK", "Main Rapper, Support Vocal"),
-                GirlGroupMember("Yeri",R.drawable.rvv_yeri, "Red Velvet", "Rapper, Support Vocal")
-        )
+        membersList = getMembersList
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = ListLightAdapter(membersList, this)
