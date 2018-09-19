1271522ddf5f6ec043745d6459f36eacb0ebd042
Chakpiwat Pacharajindawat <gameraroma@gmail.com>
2018-09-19 20:37:36 +0700

    add map activity

--- app/src/main/java/gameraromic/uitemplate/HomeActivity.kt
+++ app/src/main/java/gameraromic/uitemplate/HomeActivity.kt
@@ -8,6 +8,7 @@ import gameraromic.uitemplate.adapters.MenuListAdapter
 import gameraromic.uitemplate.exampleviews.ParallaxLightActivity
 import gameraromic.uitemplate.exampleviews.ListLightActivity
 import gameraromic.uitemplate.exampleviews.LoginLightActivity
+import gameraromic.uitemplate.exampleviews.MapsLightActivity
 import gameraromic.uitemplate.models.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -16,10 +17,12 @@ class HomeActivity : AppCompatActivity() {
     private val _loginSection: String = "Login"
     private val _listSection: String = "List"
     private val _parallaxSection: String = "Parallax"
+    private val _mapSection: String = "Map"
     private val _menuItemList = arrayOf(
             MenuItem(_loginSection, R.drawable.ic_lock_open_outline),
             MenuItem(_listSection, R.drawable.ic_format_list_bulleted),
-            MenuItem(_parallaxSection, R.drawable.icon_parallax)
+            MenuItem(_parallaxSection, R.drawable.icon_parallax),
+            MenuItem(_mapSection, R.drawable.ic_map)
             )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +42,9 @@ class HomeActivity : AppCompatActivity() {
                 }
                 _parallaxSection -> {
                     openActivity(ParallaxLightActivity::class.java)
+                }
+                _mapSection -> {
+                    openActivity(MapsLightActivity::class.java)
                 }
                 else -> {}
             }
